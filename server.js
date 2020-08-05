32a50241dafdc2fed21ab31b456363799d8ee54b
MrMoshkovitz <gal@gmail.com>
2020-08-05 13:07:25 +0300

    stating server

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const app = require("./app");
+const port = 3000;
+
+const server = http.createServer(app);
+
+server.listen(port)
+
+// var getProds = function GetProducts (products) {
+// };
+
+
+
+
+// module.exports.getProds = getProds;
