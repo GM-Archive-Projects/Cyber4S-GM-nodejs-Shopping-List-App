d18221e759222ec8b249c8774d547bffe6eb1b7e
MrMoshkovitz <gal@gmail.com>
2020-08-05 19:01:01 +0300

    as

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const express = require("express");
-const app = express();
-const { readFileSync } = require('fs');
+// Global Environment Variables
+const shoppingApp = document.getElementById("shoppingApp");
+const mainHeader = document.getElementById("mainHeader");
+const controlPanel = document.getElementById("controlPanel");
+const search = document.getElementById("search");
+const searchButton = document.getElementById("searchButton");
+const pokemosListButton = document.getElementById("pokemosListButton");
+const sortButton = document.getElementById("sortButton");
+const addButton = document.getElementById("addButton");
+const searchResults = document.getElementById("searchResults");
+const pokemonsDeck = document.getElementById("pokemonsDeck");
+const pokemonsList = document.getElementById("pokemonsList");
 
+// defaults
+axios.defaults.baseURL = "http://pokeapi.co/api/v2/pokemon";
+axios.defaults.validateStatus = (status) => status >= 200 && status < 300;
+
+// const express = require("express");
+// const app = express();
+// const { readFileSync } = require('fs');
+/*
 let products = [
     {
         id: 120,
@@ -33,22 +50,23 @@ let products = [
         price: 300,
         ammout: 3,
     }
-]
+// ]
+// */
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.status(200)
-    const homeFile = readFileSync('./home.html', 'utf8');
-    res.send(homeFile);
-  });
+// app.get("/", (req, res) => {
+//     res.status(200)
+//     const homeFile = readFileSync('./home.html', 'utf8');
+//     res.send(homeFile);
+//   });
 
 
-app.get("/products", (req, res) => {
-    res.status(200).json(products)
-    });
+// app.get("/products", (req, res) => {
+//     res.status(200).json(products)
+//     });
 
-module.exports = app;
+// module.exports = app;
 
 
 
