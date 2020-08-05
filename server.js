a92ab52acb31ecc86ea5800213b2b246d368f550
MrMoshkovitz <gal@gmail.com>
2020-08-05 20:46:37 +0300

    a

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const http = require('http');
-const app = require("./app");
 const port = 3000;
+const express = require("express");
+const app = express()
+app.use(express.json());
 
+const { readFileSync } = require('fs');
 const server = http.createServer(app);
 
+
+app.get("/", (req, res) => {
+    const homeFile = readFileSync('src/home.html', 'utf-8');
+    const styleFile = readFileSync('src/style.css')
+    res.send(homeFile);
+  });
+
+
+module.exports = app;
+
 server.listen(port)
+
--- src/app.js
+++ src/app.js
@@ -11,49 +11,15 @@ const searchResults = document.getElementById("searchResults");
 const pokemonsDeck = document.getElementById("pokemonsDeck");
 const pokemonsList = document.getElementById("pokemonsList");
 
+
+const axios = require('axios');
+
 // defaults
 axios.defaults.baseURL = "http://pokeapi.co/api/v2/pokemon";
 axios.defaults.validateStatus = (status) => status >= 200 && status < 300;
 
-// const express = require("express");
-// const app = express();
-// const { readFileSync } = require('fs');
-/*
-let products = [
-    {
-        id: 120,
-        name: "Milk",
-        price: 2,
-        ammout: 3,
-    },
-    {
-        id: 121,
-        name: "Eggs",
-        price: 3,
-        ammout: 2,
-    },
-    {
-        id: 122,
-        name: "Cheese",
-        price: 7,
-        ammout: 1,
-    },
-    {
-        id: 123,
-        name: "Bread",
-        price: 1,
-        ammout: 6,
-    },
-    {
-        id: 124,
-        name: "Cigarretes",
-        price: 300,
-        ammout: 3,
-    }
-// ]
-// */
 
-// app.use(express.json());
+
 
 // app.get("/", (req, res) => {
 //     res.status(200)
